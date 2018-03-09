70693ed3630f0675ad336b46fb4b5396a3aa70a1
Delinked <37184241+Delinked@users.noreply.github.com>
2018-03-08 22:41:59 -0500

    Add files via upload

---
+++ code/game_levels.js
@@ -0,0 +1,42 @@
+var GAME_LEVELS = [
+  
+  ["                                                                                                              y                         ",
+   "                                        y                            y                                        x                         ",
+   "                                        x                                                 xxxx     xxxx      x!x                        ",
+   "                                      x!!x                        xxxxxxx                                    x!x                        ",
+   "                                      x!!x                     xxxx     xxxx     xxxx                      xxx!x                        ",
+   "                                      x!!xxxxxxxxxx    xxx    xx           xx                                x!x                        ",
+   "                                      xx!!!!!!!!!!xx         xx             xx                      xxx      x!x                        ",
+   "                                       xxxxxxxxxx!!x         x                                               x!x                        ",
+   "                                                xx!x         x     y   y                                    xx!x                        ",
+   "                                                 x!x         x                                         xxxxxxxxx                        ",
+   "                                                 xvx         x     x   x       x                       !!!!!!!!!!!!!!!!                 ",
+   "                                                             xx   xxx  xxx   xx  x              xxxxxxxxxxxxxxxxxxxxx !                 ",
+   "                                                              xx!!!!!!!!!!!xx    xxxx         xxx                     !                 ",
+   "                                                               xxxx!!!!!xxxx      xx         xx                       !                 ",
+   "                                                                  xxxxxxx          x      xxxx                        !                 ",
+   "                                                                                   x         x                        !                 ",
+   "                                                                                   x         x                        !                 ",
+   "                                                                                   xxxx      x                        !                 ",
+   "                                                                                   x         x                        !                 ",
+   "                      y          y                                   y                     xxx                                          ",
+   "                   xxxxxxx                                                                                        y                     ",
+   "                 xx     xx     x   x                         xxxxxx     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                                ",
+   "               xx       xx     x   x                         x                                                  xxxxx                   ",
+   "     @        x         x      x   x                        x                                                                           ",
+   "    xxx      x         x       x    x                     xx                                             xxxxx        !  xxxxx          ",
+   "    x x     xx         x      xx    xx                                                                                !                 ",
+   "!!!!x xxxx!!xx         x!!!!!!xx     xx!!!!!!!!xx     xx!!!!!!!!!                             xxxxxxxxx                             xxxx",
+   "!!!!x x!!!!!!x         x!!!!!xx       xxxxxxxxxx     x!!!!!!!xx!     xxxxxxxxxxxxx xx  o o  xx                                          ",
+   "!!!!x x!!!!!!x         x!!!!!x                      xx!!!!!!xx !                    xx     xx                   y              y        ",
+   "!!!!x x!!!!!!x         x!!!!!x    y                xx!!!!!!xx  !                     xxxxxxx!!!!!!!!!!!!!!!!!!xxxx!!!!xxx!!!!xxxx!!!!!!!",
+   "!!!!x x!!!!!!x         x!!!!!xx       xxxxxxxxxxxxxx!!!!!!xx   !                                                                        ",
+   "!!!!x x!!!!!!x         x!!!!!!xxxxxxxxx!!!!!!!!!!!!!!!!!!xx    !                                                                        ",
+   "!!!!x x!!!!!!x         x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!xx     !                                                                        ",
+   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"],
+  
+
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
